047a1ca16ddf758894b44b435a12f69af2650d2c
Gary King <gwking@metabang.com>
2008-06-10 09:10:43 -0400

    Moved string-trim-if and strip-whitespace from cl-markdown to here
    darcs-hash:20080610131043-3cc5d-00f79de42f1c869d14267b88ee21312f11829271.gz

--- dev/l0-strings.lisp
+++ dev/l0-strings.lisp
@@ -36,6 +36,20 @@ each character."
 	 (incf mm))
     (= mm (length suffix))))
 
+(defun string-trim-if (predicate string &key (start 0) (end (length string)))
+  (let ((end (1- end)))
+    (loop for ch across string 
+       while (funcall predicate ch) do (incf start))
+    (when (< start end)
+      (loop for ch = (aref string end)
+         while (funcall predicate ch) do (decf end)))
+    (subseq string start (1+ end))))
+
+(defun strip-whitespace (string &key (start 0) (end (length string)))
+  (string-trim-if
+   #'whitespacep string :start start :end end))
+
+
 
 #| OR
 
