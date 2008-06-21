7309d45f6b62fad00d46cab54940f56a54b12037
Gary King <gwking@metabang.com>
2008-06-20 21:07:56 -0400

    export file-new-than-file-p; stop exporting a whole bunch of things that aren't in metatilities-base
    darcs-hash:20080621010756-3cc5d-7b77f1f76ecef4c38d10a4ba53b7062708162182.gz

--- dev/package.lisp
+++ dev/package.lisp
@@ -31,7 +31,7 @@
    #:load-op #:compile-op #:test-op 
    #:operate #:oos)
   
-  #+(or openmcl digitool)               ; ??? I think this is the right thing...
+  #+(or openmcl digitool)     ; ??? I think this is the right thing...
   (:shadow #:line #:copy-file
            #:lock-owner #:lock-name
            #:selected? #:whitespacep)
@@ -54,9 +54,8 @@
                 #:without-interrupts)
     
   (:export
-   #:apply-if-exists)
+   #:apply-if-exists
 
-  (:export
    #:defclass-property
    #:deprecated
    #:once-only
@@ -64,6 +63,8 @@
    #:eval-always
    #:with-atomic-execution
    #:handler-bind*
+
+   #:file-newer-than-file-p
    #:with-stream-from-specifier
    #:relative-pathname
    #:directory-pathname-p
@@ -75,206 +76,48 @@
    #:collect-lines
    #:collect-forms
    #:map-matching-files
-   #:collect-matching-files)
-  
-  (:export
+   #:collect-matching-files
+
    #:defclass-brief 
    #:defclass*
-   #:defcondition)
-      
-  (:export 
+   #:defcondition
+
    #:copy-array
    #:linearize-array
    #:maparray! 
    #:maparray
    #:array-row
-   #:group)
   
-  (:export 
    #:element
    #:element-type
    #:filter
    #:parent
    #:tag
-   #:find-or-create-class
    #:size
    #:root
    #:next-element
-   #:total-size)
+   #:total-size
     
-  (:export #:without-interrupts)
+   #:without-interrupts
   
-  (:export 
-   #:make-allocatable
-   #:allocate
-   #:deallocate
-   #:allocation-status 
-   #:with-object
-   #:with-objects
-   #:reallocate-instance)
-  
-  ;; views-and-windows
-  (:export
-   #:update-dialog-ui
-   #:dialog-item-value 
-   
-   #:help-text
-   
-   #:note-view-settings-changed
-   
-   #:view-scale
-   #:view-x/view-y->x/y
-   #:view-rect->rect
-   #:x/y->view-x/view-y
-   #:view-x/view-y->point
-   #:distance-x/distance-y->x/y
-   #:x/y->distance-x/distance-y
-   #:adjust-point-for-scaling
-   #:scale-x
-   #:scale-y
-   #:view-x/view-y->point
-   
-   #:view-requiring-cleanup-mixin
-   #:clean-up-view
-   
-   #:redraw
-   
-   #:begin-new-selection
-   #:end-new-selection
-   
-   #:relative-view-position
-   #:selected-items
-   #:empty-selections!
-   #:can-extend-selection-p
-   #:can-add-to-selection-p
-   #:item-selected-p
-   #:anything-selected-p
-   #:first-selected-item
-   #:select-item
-   #:unselect-item
-   #:unselect-all)
-  
-  ;; utilities
-  (:export
-   #:firstn
-   #:mapappend
-   #:find-all
-   #:find-all-if
-   #:find-all-if-not
-   #:partition
-   #:tree-find
-   #:tree-find-if
-   #:tree-remove-if
-   #:tree-map
-   #:push-end
-   
-   #:compact-array
    #:linearize-array
    #:copy-array
    #:maparray
    
-   #:nmerge-list
-   #:circular-list
-   #:make-initialized-array
    #:fixnump
-   #:object->string
-   #:float->integer
-   #:sort-using-list-order
-   #:unused-variables
-   #:lambda-list->args
-   #:car-safe
-   #:cdr-safe
-   
-   #:curry
-   #:curry-after
-   #:compose
-   #:conjoin
-   #:disjoin
-   
-   #:argmax
-   #:argmin
-   #:best-item ; see also u:minimize, u:reduce-if
-   #:singleton-or-list
-   #:very-small-number-p
-   #:+very-small-number+
+
    #:set-equal
-   #:remove-leading-quote
    
-   #:nth-elt-of-cross-product
-   #:nth-elt-of-cross-product-as-multiple-values
-   
-   #:evaluate-argument-list
-   
-   #:constant-expression-p
-   #:inspect-hyaline
-   
-   #:make-sorter
-   
-   #:cleanup-parsed-parameter
-   #:convert-clauses-into-lists
-   #:convert-clauses-into-lists*
-   #:add-class-if-necessary
-   #:add-classes-if-necessary
-   #:length-at-least-p
-   #:length-at-most-p
-   )
+   #:constant-expression-p 
   
-  ;; notifications
-  (:export 
-   #:basic-notification
-   #:register
-   #:unregister
-   #:unregister-all
-   #:post-notification
-   #:notify
-   #:with-registration
-   #:still-cares-about-notification-p)
-  
-  (:export 
-   #:spy
-   #:spyx)
-  
-  (:export
-   #:binary-search
-   #:graph-search)
-  
-  ;; macros
-  (:export
    #:muffle-redefinition-warnings
+
    #:nyi
    #:deprecated
-   #:make-obsolete
-   #:defsubst
-   #:named-lambda
-   #:deletef
-   #:removef
-   #:circular-list
-   
-   #:doplist
-   #:group
-   #:gensym0
-   #:with-conditional-open-file
-   #:with-conditional-open-files
-   #:assert*
-   #:maxf
-   #:minf
-   #:multf
-   #:incf-assoc
-   #:some*
-   #:handler-case-if
-   
-   #:gensym*
    #:with-unique-names
-   #:rebinding
+
    
-   #:\\                           ; yes, this is actually the name of a macro. ---L
-   
-   #:hcase
-   
-   #:dovect
-   #:ensure-type
-   #:with-slot-bindings
-   
+
    #:defun*
    #:defmethod*
    #:*add-check-types*
@@ -285,89 +128,20 @@
    #:funcall-if
    
    #:*file-if-exists*
-   #:with-new-file)
+   #:with-new-file
   
-  ;; graham
-  (:export
    #:with-gensyms
-   #:allf
-   #:nilf
-   #:tf
-   #:toggle!                       ; renamed toggle to toggle!
-   #:filter-values
-   #:with-array
-   #:with-matrix
-   #:with-struct
-   #:match
-   #:if-match
    
-   #:most
-   #:best
-   #:mostn
-   
-   #:map0-n
-   #:map1-n
-   #:mapa-b
-   #:map->
-   #:mapcars
-   
-   #:fn
-   #:compose
-   
-   #:concf)
-  
-  ;; files
-  (:export
    #:invalid-stream-specifier-error
 
    #:pathname-samep
-   #:file-to-list
-   #:file-newer-than-file-p
-   #:conjure-up-filename
-   #:unique-file-name-from-date
-   #:pretty-namestring-from-date
-   #:eos-namestring-from-date
-   #:short-eos-namestring-from-date
-   #:rename-file-if-present
-   #:uniquify-file-name
-   #:remove-illegal-filename-characters
-   #:shorten-filename-for-os
-   #:map-files
-   #:map-forms-in-file
-   #:map-lines-in-file 
-   #:map-lines
-   #:next-line
-   #:file-package
-   #:touch-file
-   #:ensure-filename-safe-for-os
-   #:pathname-name+type
    #:physical-pathname-directory-separator
-   #:delete-directory
-   #:directory-name-p)
-  
-  ;; dates and times
-  (:export
+
+   #:map-lines
    #:day->string 
    
    #:month->string
-   #:string->month
-   
-   #:print-date
-   #:date-string 
-   #:date-string-brief 
-   #:print-time 
-   #:print-universal-time
-   #:time-string
-   #:time-string-with-no-colons
-   #:date-and-time-string 
-   #:parse-date-and-time-string
-   #:parse-date-and-time 
-   #:print-brief-ut 
-   #:print-ut
-   #:print-time-interval 
-   #:print-brief-time-interval
-   #:parse-interval-or-never
-   
+
    #:+minutes-per-hour+
    #:+seconds-per-hour+
    #:+seconds-per-minute+
@@ -378,36 +152,9 @@
    #:format-date
    #:days-in-month
    #:day-of-year
-   #:leap-year-p)
-    
-  ;; strings
-  (:export 
-   #:string-before
-   #:string-after
-   #:string-contains-p
-   #:string->symbol
-   #:symbol->string
-   #:substring
-   #:tokenize-string
-   #:list->formatted-string)
-  
-  ;; sequences
-  (:export 
-   #:reduce-if
-   #:minimize
-   
+   #:leap-year-p
+ 
    #:dotted-pair-p
-   
-   #:flatten
-   #:power-set
-   #:all-pairs
-   #:map-combinations
-   #:combinations
-   #:remove-members
-   #:permute
-   
-   #:transpose
-   #:transpose2
    #:iterate-over-indexes
    
    #:form-symbol-in-package
@@ -449,17 +196,17 @@
    
    #:total-bytes-allocated*
    #:total-bytes-allocated
-   
+
    #:gc-time
-   
+
    #:collect-garbage*
    #:collect-garbage
-   
+
    #:make-load-form*
-   
+
    #:*development-mode* 
    #:*use-native-debugger*
-   
+
    #:with-progress-bar 
    #:progress
    #:make-progress-bar
@@ -488,30 +235,17 @@
    #:make-ui-point*
    #:process-parameters
    #:put-item-on-clipboard
-   #:inspect-thing
    #:inspect-things
    #:prompt-for
    
    #:parse-brief-slot
-   #:class-copyable-p)
+   #:class-copyable-p
   
-  (:export
-   #:length-at-least-p
-   #:length-at-most-p
-   #:length-exactly-p
-   #:same-length-p
-   #:percent-overlap)
 
-  (:export
-   #:tree-search)
-
-  ;; l0-strings
-  (:export
    #:+whitespace-characters+
    #:whitespacep
    #:string-starts-with
    #:string-ends-with
    #:string-trim-if
-   #:strip-whitespace))
-
-
+   #:strip-whitespace
+   ))
